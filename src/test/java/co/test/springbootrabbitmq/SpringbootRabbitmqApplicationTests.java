d768b8a89039c78e7b0c51b8ebbe4e0b6ad8879c
abdullahaleem0334 <abdullahaleem8766@gmail.com>
2025-12-10 03:47:24 -0800

    new changes
    commit changes

--- src/test/java/co/test/springbootrabbitmq/SpringbootRabbitmqApplicationTests.java
+++ src/test/java/co/test/springbootrabbitmq/SpringbootRabbitmqApplicationTests.java
@@ -1,6 +1,6 @@
-package co.test.springbootrabbitmq;
+//package co.test.springbootrabbitmq;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
